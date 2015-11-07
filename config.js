fc9399dc0efe4ad41392009a96e26a00cc5e8cee
megan-jo <megan.jo167@gmail.com>
2015-11-07 13:07:18 -0700

    added config

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {}
+  config.client_id = 'e41b92a2e15f48ceb2aeb7d000f02230'
+  config.client_secret  = '481364c9f8b14e0da43e6ac5ae59b04a'
+  config.redirect_uri = 'http://localhost:3000/auth/finalize'
+
+module.exports = config
